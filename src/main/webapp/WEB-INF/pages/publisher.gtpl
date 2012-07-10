1669f85b38efff0a7466be3df925cb7100382227
Glenn Saqui <glenn.saqui@gmail.com>
2012-07-10 07:40:31 +0100

    setting up publisher js

--- src/main/webapp/WEB-INF/pages/publisher.gtpl
+++ src/main/webapp/WEB-INF/pages/publisher.gtpl
@@ -8,8 +8,28 @@
   <script type="text/javascript" src="/js/jquery-1.7.2.min.js"></script>
   <script type="text/javascript" src="/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="/js/angular.js"></script>
+  <script type="text/javascript" src="/js/angular-resource.js"></script>
+  <script type="text/javascript" src="/js/publisher.js"></script>
 </head>
 <body>
 <h2>yo</h2>
+<h2>JavaScript Projects</h2>
+<table>
+  <thead>
+  <tr>
+    <th>Project</th>
+    <th>Description</th>
+    <th><a href="#/new"><i class="icon-plus-sign"></i></a></th>
+  </tr>
+  </thead>
+  <tbody>
+  <tr ng-repeat="patient in patients  | orderBy:'name'">
+    <td><a href="{{patient.name}}" target="_blank">{{patient.name}}</a></td>
+    <td>{{project.description}}</td>
+    <td>
+    </td>
+  </tr>
+  </tbody>
+</table>
 </body>
 </html>
---
+++ src/main/webapp/js/publisher.js
@@ -0,0 +1,33 @@
+// This is a module for cloud persistance in mongolab - https://mongolab.com
+angular.module('patients', ['ngResource']).
+    factory('Patient', function($resource) {
+            console.log('patient being built out')
+
+      var Patient = $resource('/patientList', {
+            update: { method: 'PUT' }
+          }
+      );
+      return Patient;
+    });
+
+
+console.log('inside of angular');
+
+angular.module('publisher', ['patients']).
+  config(function($routeProvider) {
+    $routeProvider.
+      when('/', {controller:ListCtrl, templateUrl:'list.html'}).
+      when('/publisher', {controller:ListCtrl, templateUrl:'list.html'}).
+      otherwise({redirectTo:'/publisher2'});
+  });
+
+function ListCtrl($scope, Patient) {
+    console.log('list controller');
+    $http.get('patientList').success(function(data) {
+        $scope.patients = data;
+  });
+//  $scope.projects = Patient.query();
+}
+
+
+
